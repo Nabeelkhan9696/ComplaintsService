e2b965fb5f7802667d383f6211400b20b68a01be
nabeelkhan9696 <nabeelkhan96@gmail.com>
2025-01-07 19:36:14 -0800

    Initial Commit

---
+++ src/main/java/com/payments/complaintsservice/ComplaintsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payments.complaintsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplaintsServiceApplication.class, args);
+    }
+
+}
